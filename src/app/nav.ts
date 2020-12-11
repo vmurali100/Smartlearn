7ae01d675cc3cdf1224ac22a5bdba74a2d27169f
vmurali100 <vmurali100@users.noreply.github.com>
2020-12-11 18:59:14 +0530

    added

--- src/app/nav.ts
+++ src/app/nav.ts
@@ -1,8 +1,8 @@
 export const navItems1 = [
   {
-    name: 'Smartlearn Dashboard',
-    url: '/dashboard',
-    icon: 'fa fa-tachometer'
+    name: "Smartlearn Dashboard",
+    url: "/",
+    icon: "fa fa-tachometer",
   },
   // {
   //   name: 'Requests',
@@ -10,24 +10,24 @@ export const navItems1 = [
   //   icon: 'fa fa-file-text-o',
   // },
   {
-    name: 'Admin',
-    url: '/admin/configure',
-    icon: 'fa fa-cogs',
+    name: "Admin",
+    url: "/admin/configure",
+    icon: "fa fa-cogs",
     children: [
       {
-        name: ' Manage Course',
-        url: '/admin/configure/commoditycode',
-        icon: 'fa fa-empty'
+        name: " Manage Course",
+        url: "/admin/configure/commoditycode",
+        icon: "fa fa-empty",
       },
       {
-        name: 'Add Quiz',
-        url: '/admin/configure/add-quiz',
-        icon: 'fa fa-empty'
+        name: "Add Quiz",
+        url: "/admin/configure/add-quiz",
+        icon: "fa fa-empty",
       },
       {
-        name: 'Team Result',
-        url: '/admin/configure/result-user',
-        icon: 'fa fa-empty'
+        name: "Team Result",
+        url: "/admin/configure/result-user",
+        icon: "fa fa-empty",
       },
       // {
       //   name: 'Workflows',
@@ -39,46 +39,42 @@ export const navItems1 = [
       //   url: '/admin/configure/search-workflow',
       //   icon: 'fa fa-empty'
       // }
-
-
-    ]
-  }
+    ],
+  },
 ];
 
 /** For Admin */
 export const navItems2 = [
   {
-    name: 'Dashboard',
-    url: '/admin/dashboard',
-    icon: 'fa fa-tachometer'
+    name: "Dashboard",
+    url: "/admin/dashboard",
+    icon: "fa fa-tachometer",
   },
   {
-    name: 'Configure Master Data',
-    url: '/admin/configure',
-    icon: 'fa fa-cogs',
+    name: "Configure Master Data",
+    url: "/admin/configure",
+    icon: "fa fa-cogs",
     children: [
       {
-        name: 'Commodity Code',
-        url: '/admin/configure/commoditycode',
-        icon: 'fa fa-empty'
+        name: "Commodity Code",
+        url: "/admin/configure/commoditycode",
+        icon: "fa fa-empty",
       },
       {
-        name: 'Application Access',
-        url: '/admin/configure/configure-user',
-        icon: 'fa fa-empty'
+        name: "Application Access",
+        url: "/admin/configure/configure-user",
+        icon: "fa fa-empty",
       },
       {
-        name: 'Workflows',
-        url: '/admin/configure/workgroup',
-        icon: 'fa fa-empty'
+        name: "Workflows",
+        url: "/admin/configure/workgroup",
+        icon: "fa fa-empty",
       },
       // {
       //   name: 'Workflows',
       //   url: '/admin/configure/search-workflow',
       //   icon: 'fa fa-empty'
       // }
-
-
-    ]
+    ],
   },
 ];
